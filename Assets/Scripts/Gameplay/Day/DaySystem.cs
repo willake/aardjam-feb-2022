f37ab60aa9fd029c37bb7e9b23afb211090a5750
Lincoln <willakelin@seekrtech.com>
2023-02-10 23:59:27 +0100

    update: giving the role of MainGame to DaySystem

--- Assets/Scripts/Gameplay/Day/DaySystem.cs
+++ Assets/Scripts/Gameplay/Day/DaySystem.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using System;
-using UnityEngine.Experimental.Rendering.Universal;
 using DG.Tweening;
+using Game.Gameplay.Environments;
 
 namespace Game.Gameplay
 {
@@ -16,6 +16,44 @@ namespace Game.Gameplay
     }
     public class DaySystem : MonoBehaviour
     {
+        [Header("References")]
+        public EnvironmentSystem environmentSystem;
+        public BuildingSystem buildingSystem;
 
+        public void Init()
+        {
+            buildingSystem.Init();
+        }
+
+        // prediction outcome phase
+        void StartDay()
+        {
+            //daySystem.SetState(DayState.Day);
+            // play weather animation
+        }
+
+        // building phase
+        void StartMidday()
+        {
+            //daySystem.SetState(DayState.Midday);
+            // play building animation
+            // increase building height
+        }
+
+        // prediction phase
+        void StartNight()
+        {
+            //daySystem.SetState(DayState.Night);
+            // open weather info UI
+            // wait for end day
+        }
+
+        private void Update()
+        {
+            //if (Input.GetKeyDown(KeyCode.A)) daySystem.SetState(DayState.Day);
+            //if (Input.GetKeyDown(KeyCode.S)) daySystem.SetState(DayState.Midday);
+            //if (Input.GetKeyDown(KeyCode.D)) daySystem.SetState(DayState.Night);
+            if (Input.GetKeyDown(KeyCode.Q)) buildingSystem.IncreaseFloor();
+        }
     }
 }
--- Assets/Scripts/Gameplay/MainGame/MainGame.cs
+++ Assets/Scripts/Gameplay/MainGame/MainGame.cs
@@ -9,47 +9,14 @@ namespace Game.Gameplay
     {
         [Header("References")]
         public DaySystem daySystem;
-        public BuildingSystem buildingSystem;
         void Start()
         {
             UIManager.instance.OpenUI(AvailableUI.GameHUDPanel);
-            buildingSystem.Init();
         }
 
         void PlayIntro()
         {
 
-        }
-
-        // prediction outcome phase
-        void StartDay()
-        {
-            //daySystem.SetState(DayState.Day);
-            // play weather animation
-        }
-
-        // building phase
-        void StartMidday()
-        {
-            //daySystem.SetState(DayState.Midday);
-            // play building animation
-            // increase building height
-        }
-
-        // prediction phase
-        void StartNight()
-        {
-            //daySystem.SetState(DayState.Night);
-            // open weather info UI
-            // wait for end day
-        }
-
-        private void Update()
-        {
-            //if (Input.GetKeyDown(KeyCode.A)) daySystem.SetState(DayState.Day);
-            //if (Input.GetKeyDown(KeyCode.S)) daySystem.SetState(DayState.Midday);
-            //if (Input.GetKeyDown(KeyCode.D)) daySystem.SetState(DayState.Night);
-            if (Input.GetKeyDown(KeyCode.Q)) buildingSystem.IncreaseFloor();
         }
     }
 }
