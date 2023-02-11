159995a436acc5a0885c73746134f66c857210a2
Lincoln <willakelin@seekrtech.com>
2023-02-11 16:45:45 +0100

    update: rename variables

--- Assets/Scripts/Gameplay/Building/Villager.cs
+++ Assets/Scripts/Gameplay/Building/Villager.cs
@@ -13,29 +13,29 @@ namespace Game.Gameplay
         [Header("Settings")]
         public GameObject homePointPrefab;
 
-        private float towerXOffset = 2;
-        private float edgeXOffset = 1;
-        private float randomTargetOffset = 1f;
-        private float YSpawn = -2.5f;
+        private float _towerXOffset = 2;
+        private float _edgeXOffset = 1;
+        private float _randomTargetOffset = 1f;
+        private float _YSpawn = -2.5f;
 
         public event System.Action onVillagerReachedTower = delegate { };
-        private Vector2 targetPos;
-        private bool movingToTower = false;
-        private bool reachedTower = false;
-        private float speed;
+        private Vector2 _targetPos;
+        private bool _movingToTower = false;
+        private bool _reachedTower = false;
+        private float _speed;
 
         //Maybe seperate some other time
-        private float normalSpeed = 3f;
-        private float lazySpeed = 1.5f;
-        private float fastSpeed  = 4.5f;
+        private float _normalSpeed = 3f;
+        private float _lazySpeed = 1.5f;
+        private float _fastSpeed = 4.5f;
 
-        public void Init(Transform _homePointHolder, Transform _target)
+        public void Init(Transform homePointHolder, Transform target)
         {
             Type = PickRandomType();
-            HomePoint = Instantiate(homePointPrefab, _homePointHolder);
+            HomePoint = Instantiate(homePointPrefab, homePointHolder);
             HomePoint.transform.position = PickRandomSpawnLoc();
             transform.position = HomePoint.transform.position;
-            targetPos = _target.position;
+            _targetPos = target.position;
         }
 
         private VillagerType PickRandomType()
@@ -43,17 +43,17 @@ namespace Game.Gameplay
             float randomType = Random.Range(0f, 1f);
             if (randomType <= 0.33f)
             {
-                speed = normalSpeed;
+                _speed = _normalSpeed;
                 return VillagerType.Normal;
             }
             else if (randomType >= 0.66f)
             {
-                speed = lazySpeed;
+                _speed = _lazySpeed;
                 return VillagerType.Lazy;
             }
             else
             {
-                speed = fastSpeed;
+                _speed = _fastSpeed;
                 return VillagerType.Fast;
             }
         }
@@ -61,19 +61,19 @@ namespace Game.Gameplay
         private Vector2 PickRandomSpawnLoc()
         {
             Vector2 worldSpaceMax = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0));
-            float randomXScreenSpace = Random.Range(towerXOffset, worldSpaceMax.x - edgeXOffset);
-            Vector2 spawnPoint = Random.value >= 0.5f ? new Vector2(randomXScreenSpace, YSpawn) : new Vector2(-randomXScreenSpace, YSpawn);
+            float randomXScreenSpace = Random.Range(_towerXOffset, worldSpaceMax.x - _edgeXOffset);
+            Vector2 spawnPoint = Random.value >= 0.5f ? new Vector2(randomXScreenSpace, _YSpawn) : new Vector2(-randomXScreenSpace, _YSpawn);
             return spawnPoint;
         }
 
         private void Update()
         {
-            if (movingToTower && !reachedTower)
+            if (_movingToTower && !_reachedTower)
             {
-                if (targetPos == null || !HomePoint) return;
+                if (_targetPos == null || !HomePoint) return;
 
-                float step = speed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+                float step = _speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, _targetPos, step);
             }
         }
 
@@ -81,7 +81,7 @@ namespace Game.Gameplay
         {
             if (other.gameObject.tag == Consts.TOWERTAG)
             {
-                if (!reachedTower)
+                if (!_reachedTower)
                 {
                     ReachedTower();
                 }
@@ -91,39 +91,39 @@ namespace Game.Gameplay
         public void StartMoveTowardTower()
         {
             transform.position = HomePoint.transform.position;
-            reachedTower = false;
-            movingToTower = true;
+            _reachedTower = false;
+            _movingToTower = true;
             AnimateWalk();
         }
 
         public void ReachedTower()
         {
             DOTween.KillAll();
-            reachedTower = true;
+            _reachedTower = true;
             onVillagerReachedTower.Invoke();
         }
 
         public void ReturnHome()
         {
             transform.position = HomePoint.transform.position;
-            reachedTower = false;
-            movingToTower = false;
+            _reachedTower = false;
+            _movingToTower = false;
             DOTween.KillAll();
         }
 
         public void AnimateWalk()
         {
             DOTween.Sequence()
-                .Append(transform.DOMoveY(YSpawn + 0.1f, 0.1f))
-                .Append(transform.DOMoveY(YSpawn, 0.05f))
+                .Append(transform.DOMoveY(_YSpawn + 0.1f, 0.1f))
+                .Append(transform.DOMoveY(_YSpawn, 0.05f))
                 .SetLoops(-1);
         }
 
         public void AnimateWork()
         {
             DOTween.Sequence()
-                .Append(transform.DOMoveY(YSpawn + Random.Range(0.5f, 1.5f), Random.Range(0.1f, 0.3f)))
-                .Append(transform.DOMoveY(YSpawn, 0.1f))
+                .Append(transform.DOMoveY(_YSpawn + Random.Range(0.5f, 1.5f), Random.Range(0.1f, 0.3f)))
+                .Append(transform.DOMoveY(_YSpawn, 0.1f))
                 .SetLoops(-1);
         }
 
