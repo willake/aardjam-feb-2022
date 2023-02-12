5ccadb849fc282639662960e479018559f5c23d7
Lincoln <willakelin@seekrtech.com>
2023-02-12 18:47:57 +0100

    update: finalize music panel

--- Assets/Scripts/UI/Panels/MenuPanel/MenuPanel.cs
+++ Assets/Scripts/UI/Panels/MenuPanel/MenuPanel.cs
@@ -17,6 +17,8 @@ namespace Game.UI
 
         [Title("References")]
         public WDTextButton btnPlay;
+        public WDTextButton btnSettings;
+        public WDTextButton btnExit;
 
         void Start()
         {
@@ -24,6 +26,21 @@ namespace Game.UI
                 .OnClickObservable
                 .ObserveOnMainThread()
                 .Subscribe(_ => SwitchToMainGame())
+                .AddTo(this);
+
+            btnSettings
+                .OnClickObservable
+                .ObserveOnMainThread()
+                .Subscribe(async _ =>
+                {
+                    await UIManager.instance.OpenUIAsync(AvailableUI.SettingsPanel);
+                })
+                .AddTo(this);
+
+            btnExit
+                .OnClickObservable
+                .ObserveOnMainThread()
+                .Subscribe(_ => GameManager.instance.ExitGame())
                 .AddTo(this);
 
             btnPlay.SetText("Play");
--- Assets/Scripts/UI/Panels/SettingsPanel/SettingsPanel.cs
+++ Assets/Scripts/UI/Panels/SettingsPanel/SettingsPanel.cs
@@ -1,17 +1,24 @@
 using UniRx;
 using System;
 using Cysharp.Threading.Tasks;
-using Game.Events;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using DG.Tweening;
 using UnityEngine.UI;
+using Game.Screens;
+using System.Linq;
+using Game.Audios;
 
 namespace Game.UI
 {
     public class SettingsPanel : UIPanel
     {
         public override AvailableUI Type { get => AvailableUI.SettingsPanel; }
+        private Lazy<ScreenAspectManager> _screenAspectManager = new Lazy<ScreenAspectManager>(
+            () => DIContainer.instance.GetObject<ScreenAspectManager>(),
+            true
+        );
+        protected ScreenAspectManager ScreenAspectManager { get => _screenAspectManager.Value; }
 
         [Title("References")]
         public Transform panel;
@@ -21,6 +28,83 @@ namespace Game.UI
         public Slider sliderSFX;
         public WDTextButton btnApply;
         public WDTextButton btnClose;
+
+        private ScreenMode _screenMode;
+        private Resolution[] _resolutions;
+        private int _resolutionsIndex = 0;
+
+        private void Start()
+        {
+            btnScreenmode
+                .OnClickObservable
+                .ObserveOnMainThread()
+                .Subscribe(_ =>
+                {
+                    _screenMode = _screenMode == ScreenMode.FullScreen
+                        ? ScreenMode.Windowed
+                        : ScreenMode.FullScreen;
+
+                    UpdateButtons();
+                })
+                .AddTo(this);
+
+            btnResolution
+                .OnClickObservable
+                .ObserveOnMainThread()
+                .Subscribe(_ =>
+                {
+                    int newIndex = _resolutionsIndex - 1;
+
+                    if (newIndex < 0) newIndex = _resolutions.Length - 1;
+
+                    _resolutionsIndex = newIndex;
+
+                    UpdateButtons();
+                })
+                .AddTo(this);
+
+            sliderMusic
+                .OnValueChangedAsObservable()
+                .ObserveOnMainThread()
+                .Subscribe(v => AudioManager.instance.SetMusicVolumeByPercentage(v))
+                .AddTo(this);
+
+            sliderSFX
+                .OnValueChangedAsObservable()
+                .ObserveOnMainThread()
+                .Subscribe(v => AudioManager.instance.SetSFXVolumeByPercentage(v))
+                .AddTo(this);
+
+            btnApply
+                .OnClickObservable
+                .ObserveOnMainThread()
+                .Subscribe(_ => ApplyScreenOption())
+                .AddTo(this);
+
+            btnClose
+                .OnClickObservable
+                .ObserveOnMainThread()
+                .Subscribe(_ => UIManager.instance.PrevAsync().Forget())
+                .AddTo(this);
+        }
+
+        private void Init()
+        {
+            WrappedResolution resolution = ScreenAspectManager.Resolution;
+            _resolutions = Screen.resolutions.Where(x => x.refreshRate == resolution.refreshRate).ToArray();
+            _resolutionsIndex = _resolutions
+                .ToList().FindIndex(x => x.width == resolution.width && x.height == resolution.height);
+
+            if (_resolutionsIndex < 0) _resolutionsIndex = Screen.resolutions.Length - 1;
+
+            _screenMode = ScreenAspectManager.ScreenMode;
+
+            sliderMusic.maxValue = 1.0f;
+            sliderMusic.minValue = 0.0f;
+            sliderSFX.maxValue = 1.0f;
+            sliderSFX.minValue = 0.0f;
+        }
+
 
         public override WDButton[] GetSelectableButtons()
         {
@@ -34,10 +118,18 @@ namespace Game.UI
 
         public override void Open()
         {
+            Init();
+
+            UpdateButtons();
+
             gameObject.SetActive(true);
         }
         public override async UniTask OpenAsync()
         {
+            Init();
+
+            UpdateButtons();
+
             gameObject.SetActive(true);
             await UniTask.RunOnThreadPool(() => { });
         }
@@ -49,6 +141,61 @@ namespace Game.UI
         {
             gameObject.SetActive(false);
             await UniTask.RunOnThreadPool(() => { });
+        }
+
+        private void UpdateButtons()
+        {
+            btnScreenmode.SetText(GetScreenModeText(_screenMode));
+
+#if !UNITY_WEBGL
+            if (_screenMode == ScreenMode.FullScreen)
+            {
+                btnResolution.SetTextColor(new Color(0.6f, 0.6f, 0.6f, 1));
+                btnResolution.SetIsInteractable(false);
+            }
+            else
+            {
+                btnResolution.SetTextColor(new Color(0.31f, 0.31f, 0.31f, 1));
+                btnResolution.SetIsInteractable(true);
+            }
+#else
+            btnResolution.SetTextColor(new Color(0.6f, 0.6f, 0.6f, 1));
+            btnResolution.SetIsInteractable(false);
+            btnScreenmode.SetTextColor(new Color(0.6f, 0.6f, 0.6f, 1));
+            btnScreenmode.SetIsInteractable(false);
+#endif
+
+            Resolution resolution = _resolutions[_resolutionsIndex];
+            btnResolution.SetText($"{resolution.width} x {resolution.height}");
+
+            sliderMusic.value = AudioManager.instance.MusicVolumePercentage;
+            sliderSFX.value = AudioManager.instance.SFXVolumePercentage;
+        }
+
+        private void ApplyScreenOption()
+        {
+#if !UNITY_WEBGL
+            if (_screenMode == ScreenMode.FullScreen)
+            {
+                ScreenAspectManager.SetFullScreen();
+            }
+            else
+            {
+                ScreenAspectManager.SetWindow(_resolutions[_resolutionsIndex]);
+            }
+#endif
+        }
+
+        private string GetScreenModeText(ScreenMode mode)
+        {
+            switch (mode)
+            {
+                case ScreenMode.FullScreen:
+                default:
+                    return "FullScreen";
+                case ScreenMode.Windowed:
+                    return "Windowed";
+            }
         }
 
         private void OnDestroy()
